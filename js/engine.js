786cfcf8dd6a95deb4f5e81d28def26203e96cf8
Jesus Mejia <jesusmejded@gmail.com>
2018-09-20 22:43:49 -0500

    engine

---
+++ js/engine.js
@@ -0,0 +1,40 @@
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("content-banner");
+
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+
+  slides[slideIndex-1].style.display = "table";  
+}
+
+function initSlides(){
+  showSlides(slideIndex);
+  
+  setInterval(function() {
+    slideIndex += 1
+    showSlides(slideIndex);
+  }, 3000)
+}
+
+function displayMenu() {
+  var menu = document.getElementById('menu-menu-principal');
+  console.log("sfasdfasdf");
+  if (menu.style.display == 'block') {
+    menu.style.display = 'none'
+  }else{
+    menu.style.display = 'block'
+  }
+}
