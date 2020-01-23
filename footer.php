286ad7bff173f062cce5b4a75c59610e873395c5
Jesús Mejía <jesusmejded@gmail.com>
2020-01-22 22:45:55 -0600

    footer menu

--- footer.php
+++ footer.php
@@ -1,7 +1,29 @@
 
     <footer>
-        &copy; <?php echo date("Y"); ?> Bi-level Optimization
+
+
+        <div class="footer-msg">
+            <b>
+                &copy; <?php echo date("Y"); ?> Bi-level Optimization
+            </b>
+        </div>
+        <div class="social-icons-bottom">
+             <?php if ( function_exists('cn_social_icon') ) echo cn_social_icon(); ?> 
+        </div>
+        <div class="menu-wraper-bottom">
+        <?php
+        wp_nav_menu(
+            array(
+            'theme_location' => 'bottom',
+            'container' => 'div',
+            'container_class' => 'menu-bottom',
+            )
+        );
+         ?>
         <?php wp_footer(); ?>
+        </div>
+
+        <div style="clear: both;"></div>
     </footer>
 </body>
 <script type="text/javascript">
